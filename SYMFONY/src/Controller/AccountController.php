b21213e53903d454c5dfc3f3a750f4e3b77a21d3
Joron Paul <paul.joron@orange.fr>
2023-03-09 19:49:55 +0100

    change

--- SYMFONY/src/Controller/AccountController.php
+++ SYMFONY/src/Controller/AccountController.php
@@ -1,18 +1,26 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class AccountController extends AbstractController
 {
+
     #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    public function show(UserRepository $userRepository): Response
     {
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
-        ]);
+        
+        return $this->render('account/index.html.twig');
     }
+
+
+
+
+
+
+
 }
--- SYMFONY/src/Controller/SecurityController.php
+++ SYMFONY/src/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('account');
+            return $this->redirectToRoute('app_account');
         }
 
         // get the login error if there is one
